0f865b361c2099f525866d2c895f49fe599ed088
caboky31 Bernard <caboky31@hotmail.com>
2018-09-17 18:12:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,14 +28,17 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 array.shift();
 return array
  }
+ 
  function removeElementFromBeginningOfArray(array){
 array = array.slice(1);
 return array
  }
+ 
  function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array
 }
+
  function removeElementFromEndOfArray(array){
  return array.slice(0, array.length - 1)
  }
